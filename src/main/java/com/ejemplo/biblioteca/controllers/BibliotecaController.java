22af01fd647467f1a460a0851909b31b7a7b790f
atommnava <atommunoznava@gmail.com>
2026-03-13 13:20:56 -0600

    INYECCION DE DEPENDENCIAS CLASE MODEL Y REPOSITORY

--- src/main/java/com/ejemplo/biblioteca/controllers/BibliotecaController.java
+++ src/main/java/com/ejemplo/biblioteca/controllers/BibliotecaController.java
@@ -1,6 +1,8 @@
 package com.ejemplo.biblioteca.controllers;
 
 import com.ejemplo.biblioteca.service.ServicioLibro;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ public class BibliotecaController {
 
     public BibliotecaController(ServicioLibro servicioLibro) {
         this.servicioLibro = servicioLibro;
+    }
+
+    @GetMapping("/{titulo}")
+    public String dameLibro(@PathVariable String titulo) {
+        return servicioLibro.buscaLibro(titulo);
     }
 }
